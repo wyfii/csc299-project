ac931b6910c4c2b73682e24b1bd6b887625e6d58
wyfimoney <wyfiemail@gmail.com>
2025-10-02 02:06:33 -0500

    d

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,8 @@ const config = {
         mono: ["var(--font-geist-mono)", "ui-monospace", "monospace"],
       },
       colors: {
+        'trench-orange': '#ff6b00',
+        'orange-500': '#ff8533',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
